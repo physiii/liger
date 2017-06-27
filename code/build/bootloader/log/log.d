75e30527b04fc91e658f524dbf8fdb9370f72ab9
Andy <andy@pyfi.org>
2017-06-27 16:27:06 -0500

    update

--- code/build/bootloader/log/log.d
+++ code/build/bootloader/log/log.d
@@ -1,22 +1,22 @@
-log.o: /home/physiii/esp/esp-idf/components/log/./log.c \
- /home/physiii/esp/esp-idf/components/esp32/include/esp_attr.h \
- /home/physiii/esp/esp-idf/components/esp32/include/xtensa/hal.h \
- /home/physiii/esp/esp-idf/components/soc/esp32/include/soc/soc.h \
- /home/physiii/esp/esp-idf/components/esp32/include/esp_assert.h \
- /home/physiii/esp/esp-idf/components/log/include/esp_log.h \
+log.o: /home/physiii/code/esp-idf/components/log/./log.c \
+ /home/physiii/code/esp-idf/components/esp32/include/esp_attr.h \
+ /home/physiii/code/esp-idf/components/esp32/include/xtensa/hal.h \
+ /home/physiii/code/esp-idf/components/soc/esp32/include/soc/soc.h \
+ /home/physiii/code/esp-idf/components/esp32/include/esp_assert.h \
+ /home/physiii/code/esp-idf/components/log/include/esp_log.h \
  /home/physiii/circuits/liger/code/build/bootloader/../include/sdkconfig.h \
- /home/physiii/esp/esp-idf/components/esp32/include/rom/ets_sys.h
+ /home/physiii/code/esp-idf/components/esp32/include/rom/ets_sys.h
 
-/home/physiii/esp/esp-idf/components/esp32/include/esp_attr.h:
+/home/physiii/code/esp-idf/components/esp32/include/esp_attr.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/xtensa/hal.h:
+/home/physiii/code/esp-idf/components/esp32/include/xtensa/hal.h:
 
-/home/physiii/esp/esp-idf/components/soc/esp32/include/soc/soc.h:
+/home/physiii/code/esp-idf/components/soc/esp32/include/soc/soc.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/esp_assert.h:
+/home/physiii/code/esp-idf/components/esp32/include/esp_assert.h:
 
-/home/physiii/esp/esp-idf/components/log/include/esp_log.h:
+/home/physiii/code/esp-idf/components/log/include/esp_log.h:
 
 /home/physiii/circuits/liger/code/build/bootloader/../include/sdkconfig.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/rom/ets_sys.h:
+/home/physiii/code/esp-idf/components/esp32/include/rom/ets_sys.h:
--- code/build/mbedtls/library/cipher_wrap.d
+++ code/build/mbedtls/library/cipher_wrap.d
@@ -1,128 +1,128 @@
 library/cipher_wrap.o: \
- /home/physiii/esp/esp-idf/components/mbedtls/library/cipher_wrap.c \
- /home/physiii/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h \
+ /home/physiii/code/esp-idf/components/mbedtls/library/cipher_wrap.c \
+ /home/physiii/code/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h \
  /home/physiii/circuits/liger/code/build/include/sdkconfig.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/check_config.h \
- /home/physiii/esp/esp-idf/components/newlib/include/limits.h \
- /home/physiii/esp/esp-idf/components/newlib/include/newlib.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/cipher_internal.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/cipher.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/aes.h \
- /home/physiii/esp/esp-idf/components/newlib/include/stdint.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/_default_types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/features.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/_intsup.h \
- /home/physiii/esp/esp-idf/components/mbedtls/port/include/aes_alt.h \
- /home/physiii/esp/esp-idf/components/esp32/include/hwcrypto/aes.h \
- /home/physiii/esp/esp-idf/components/esp32/include/esp_types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/cdefs.h \
- /home/physiii/esp/esp-idf/components/esp32/include/rom/aes.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/arc4.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/camellia.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/des.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/blowfish.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/gcm.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/ccm.h \
- /home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/platform.h \
- /home/physiii/esp/esp-idf/components/newlib/include/stdio.h \
- /home/physiii/esp/esp-idf/components/newlib/include/_ansi.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/config.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/ieeefp.h \
- /home/physiii/esp/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/reent.h \
- /home/physiii/esp/esp-idf/components/newlib/include/_ansi.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/_types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/_types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/lock.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/types.h \
- /home/physiii/esp/esp-idf/components/newlib/include/sys/stdio.h \
- /home/physiii/esp/esp-idf/components/newlib/include/stdlib.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/stdlib.h \
- /home/physiii/esp/esp-idf/components/newlib/include/alloca.h \
- /home/physiii/esp/esp-idf/components/newlib/include/time.h \
- /home/physiii/esp/esp-idf/components/newlib/include/machine/time.h
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/check_config.h \
+ /home/physiii/code/esp-idf/components/newlib/include/limits.h \
+ /home/physiii/code/esp-idf/components/newlib/include/newlib.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/cipher_internal.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/cipher.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/aes.h \
+ /home/physiii/code/esp-idf/components/newlib/include/stdint.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/_default_types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/features.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/_intsup.h \
+ /home/physiii/code/esp-idf/components/mbedtls/port/include/aes_alt.h \
+ /home/physiii/code/esp-idf/components/esp32/include/hwcrypto/aes.h \
+ /home/physiii/code/esp-idf/components/esp32/include/esp_types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/cdefs.h \
+ /home/physiii/code/esp-idf/components/esp32/include/rom/aes.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/arc4.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/camellia.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/des.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/blowfish.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/gcm.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/ccm.h \
+ /home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/platform.h \
+ /home/physiii/code/esp-idf/components/newlib/include/stdio.h \
+ /home/physiii/code/esp-idf/components/newlib/include/_ansi.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/config.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/ieeefp.h \
+ /home/physiii/code/esp-idf/components/esp32/include/xtensa/config/core-isa.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/reent.h \
+ /home/physiii/code/esp-idf/components/newlib/include/_ansi.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/_types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/_types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/lock.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/types.h \
+ /home/physiii/code/esp-idf/components/newlib/include/sys/stdio.h \
+ /home/physiii/code/esp-idf/components/newlib/include/stdlib.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/stdlib.h \
+ /home/physiii/code/esp-idf/components/newlib/include/alloca.h \
+ /home/physiii/code/esp-idf/components/newlib/include/time.h \
+ /home/physiii/code/esp-idf/components/newlib/include/machine/time.h
 
-/home/physiii/esp/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h:
+/home/physiii/code/esp-idf/components/mbedtls/port/include/mbedtls/esp_config.h:
 
 /home/physiii/circuits/liger/code/build/include/sdkconfig.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/check_config.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/check_config.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/limits.h:
+/home/physiii/code/esp-idf/components/newlib/include/limits.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/newlib.h:
+/home/physiii/code/esp-idf/components/newlib/include/newlib.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/cipher_internal.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/cipher_internal.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/cipher.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/cipher.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/aes.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/aes.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/stdint.h:
+/home/physiii/code/esp-idf/components/newlib/include/stdint.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/_default_types.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/_default_types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/features.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/features.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/_intsup.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/_intsup.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/port/include/aes_alt.h:
+/home/physiii/code/esp-idf/components/mbedtls/port/include/aes_alt.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/hwcrypto/aes.h:
+/home/physiii/code/esp-idf/components/esp32/include/hwcrypto/aes.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/esp_types.h:
+/home/physiii/code/esp-idf/components/esp32/include/esp_types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/cdefs.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/cdefs.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/rom/aes.h:
+/home/physiii/code/esp-idf/components/esp32/include/rom/aes.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/arc4.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/arc4.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/camellia.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/camellia.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/des.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/des.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/blowfish.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/blowfish.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/gcm.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/gcm.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/ccm.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/ccm.h:
 
-/home/physiii/esp/esp-idf/components/mbedtls/include/mbedtls/platform.h:
+/home/physiii/code/esp-idf/components/mbedtls/include/mbedtls/platform.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/stdio.h:
+/home/physiii/code/esp-idf/components/newlib/include/stdio.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/_ansi.h:
+/home/physiii/code/esp-idf/components/newlib/include/_ansi.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/config.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/config.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/ieeefp.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/ieeefp.h:
 
-/home/physiii/esp/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
+/home/physiii/code/esp-idf/components/esp32/include/xtensa/config/core-isa.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/reent.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/reent.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/_ansi.h:
+/home/physiii/code/esp-idf/components/newlib/include/_ansi.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/_types.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/_types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/_types.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/_types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/lock.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/lock.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/types.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/types.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/types.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/sys/stdio.h:
+/home/physiii/code/esp-idf/components/newlib/include/sys/stdio.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/stdlib.h:
+/home/physiii/code/esp-idf/components/newlib/include/stdlib.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/stdlib.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/stdlib.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/alloca.h:
+/home/physiii/code/esp-idf/components/newlib/include/alloca.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/time.h:
+/home/physiii/code/esp-idf/components/newlib/include/time.h:
 
-/home/physiii/esp/esp-idf/components/newlib/include/machine/time.h:
+/home/physiii/code/esp-idf/components/newlib/include/machine/time.h:
