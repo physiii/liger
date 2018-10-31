fae108d1d62349b5d734320b899c56572cbde565
andy payne <andy@pyfi.org>
2018-10-31 11:16:27 -0500

    update

--- code/main/services/dimmer.c
+++ code/main/services/dimmer.c
@@ -68,8 +68,9 @@ void set_brightness(int level) {
       gpio_set_level(TRIAC_IO, 0);
     }
   }
-  level = max_brightness - level;
-  if (level) {
+
+  if (level > min_brightness) {
+    level = max_brightness - level;
     triac_delay = min_triac_delay + max_triac_delay*((double)level/max_brightness);
   } else gpio_set_level(TRIAC_IO, 0);
   printf("set brightness to %d\n",current_brightness);
